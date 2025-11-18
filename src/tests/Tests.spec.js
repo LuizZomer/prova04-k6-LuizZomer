71783fa02f2f2903290668d71af13a0c1341c104
Luiz Felipe Silveira Zomer <luiz.282422@alunosatc.edu.br>
2025-11-18 19:11:53 -0300

    first commit

---
+++ src/tests/Tests.spec.js
@@ -0,0 +1,49 @@
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend, Rate } from 'k6/metrics';
+
+export const getContactsDuration = new Trend('get_contacts', true);
+export const RateContentOK = new Rate('content_OK');
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.25'],
+    get_contacts: ['p(90)<6800'],
+    content_OK: ['rate>0.75']
+  },
+  stages: [
+    { duration: '1s', target: 7 },
+    { duration: '3m29s', target: 92 }
+  ]
+};
+
+export function handleSummary(data) {
+  return {
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
+  };
+}
+
+export default function () {
+  const baseUrl = 'https://test-api.k6.io/public/crocodiles/';
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const OK = 200;
+
+  const res = http.get(`${baseUrl}`, params);
+
+  getContactsDuration.add(res.timings.duration);
+
+  RateContentOK.add(res.status === OK);
+
+  check(res, {
+    'GET Contacts - Status 200': r => r.status === OK
+  });
+}
